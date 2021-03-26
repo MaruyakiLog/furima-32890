e8f7f30618877e33b4fde312c4411d32db4ee264
MaruyakiLog <auklm0015@gmail.com>
2021-03-26 21:47:19 +0900

    バリデーション設定(password)
    ・英数字混在

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
   validates :nickname,        presence: true
   validates :last_name,       presence: true
