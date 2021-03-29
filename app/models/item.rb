09adb424f39a1c685dcd0e1fa090e024ac74f13a
MaruyakiLog <auklm0015@gmail.com>
2021-03-29 22:50:07 +0900

    バリデーションメッセージ修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,17 +10,18 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :day
   
-  HANKAKU_NUM_REGEX = /\A[0-9]+\z/.freeze
+  HANKAKU_NUM_REGEX = /\A[0-9]+$\z/.freeze
   
   with_options presence:true do
     validates :image
     validates :name
-    validates :category_id, numericality: {other_than: 1}
-    validates :condition_id
-    validates :responsibility_id
-    validates :prefecture_id
-    validates :day_id
-    validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: {with: HANKAKU_NUM_REGEX, message: 'には半角数字で入力してください'}
+    validates :description
+    validates :category_id, numericality: {other_than: 1, message: 'Select'}
+    validates :condition_id, numericality: {other_than: 1, message: 'Select'}
+    validates :responsibility_id, numericality: {other_than: 1, message: 'Select'}
+    validates :prefecture_id, numericality: {other_than: 1, message: 'Select'}
+    validates :day_id, numericality: {other_than: 1, message: 'Select'}
+    validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range'}, format: {with: HANKAKU_NUM_REGEX, message: 'Half-width number' }
     validates :user
   end
 end
