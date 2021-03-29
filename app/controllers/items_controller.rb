c0ef343ff6c673e30c0e3c687b4590c130b2fe9f
MaruyakiLog <auklm0015@gmail.com>
2021-03-30 02:20:34 +0900

    単体テストコード実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,11 @@
 class ItemsController < ApplicationController
   def index
   end
+
   def new
     @item = Item.new
   end
+
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -16,6 +18,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :responsibility_id, :prefecture_id, :day_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :responsibility_id, :prefecture_id,
+                                 :day_id, :price).merge(user_id: current_user.id)
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.name }
     email                 { Faker::Internet.free_email }
-    # password = Faker::Internet.password( min_length: 6, mix_case: true)
     password = '1a' + Faker::Internet.password(min_length: 4)
     password              { password }
     password_confirmation { password }
