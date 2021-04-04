bda521cc340be03559928f987ff9d374875c4c77
MaruyakiLog <auklm0015@gmail.com>
2021-04-04 15:16:08 +0900

    レビュー指摘箇所の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ require 'responsibility'
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :user_check, only: [:edit, :update]
+  before_action :user_check, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order(id: 'DESC')
