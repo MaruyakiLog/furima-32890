44fb6d429f865a46b4f5947cb43b9262594267e4
MaruyakiLog <auklm0015@gmail.com>
2021-04-22 19:01:52 +0900

    DB設定修正

--- db/migrate/20210328111937_create_items.rb
+++ db/migrate/20210328111937_create_items.rb
@@ -1,5 +1,5 @@
 class CreateItems < ActiveRecord::Migration[6.0]
-  def up
+  def change
     create_table :items do |t|
       t.string     :name,              null: false
       t.text       :description,       null: false
@@ -12,7 +12,5 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.references :user,              null: false, foreign_key: true
       t.timestamps
     end
-  end
-  def down
   end
 end
